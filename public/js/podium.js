17385e28873f90d8d77b90257532cf60762ee669
Sukhdev Shah <sukhdev.s.shah@gmail.com>
2012-07-23 15:19:15 +0100

    Updated podium graphic rather than drawing boxes Restructured the code

--- public/js/podium.js
+++ public/js/podium.js
@@ -6,117 +6,89 @@ var log = function (m) {
 	}
 };
 
-window.requestAnimationFrame = (function () {
-	return window.requestAnimationFrame  ||    
-			window.webkitRequestAnimationFrame ||    
-			window.mozRequestAnimationFrame ||    
-			window.oRequestAnimationFrame  ||    
-			window.msRequestAnimationFrame  ||    
-			function(callback, element) {   
-				window.setTimeout(callback, 1000 / 60);   
-			};   
-}());
-
-
-var Podium = (function () {
-	
-	var doc     = document,
-		canvas  = doc.getElementById('podium'),
-		ctx     = canvas.getContext('2d');
-	
-	// only draw when image has loaded
-//	var render = function () {
-//
-//		// set background stuff
-//		var bgReady = false;
-//		var bgImage = new Image();
-//		bgImage.onload = function () {
-//			bgReady = true;
-//		};
-//		bgImage.src = "/img/podium.png";
-//		
-//		if (bgReady) {
-//			ctx.drawImage(bgImage, 0, 0);
-//		} else {
-//			log("background not loaded!");
-//		}
-//		
-//	};
-	
-	var drawPodium = function () {
-		ctx.font = "90px Arial";
-		ctx.fillText("1", 625, 450);
-		ctx.fillText("2", 425, 560);
-		ctx.fillText("3", 825, 580);
+var Podium = {
 		
-		ctx.strokeRect(350, 450, 200, 150);
-		ctx.strokeRect(550, 350, 200, 250);
-		ctx.strokeRect(750, 500, 200, 100);
-		
-	};
+	doc : null,
+	canvas : null,
+	ctx : null,
+	canvasY : 0,
+	canvasMaxY : 500,
+	width : 960,
+    height : 1100,
 	
-	var drawImages = function () {
+	init : function () {
+		Podium.doc = document;
+		Podium.canvas = Podium.doc.getElementById('podium');
+		Podium.ctx = Podium.canvas.getContext('2d');
 		
-		ctx.font = "18px Arial";
-		// 1st place
-		var firstReady = false;
-		var firstImage = new Image();
-		firstImage.onload = function () {
-			ctx.drawImage(firstImage, 580, 100);
-		};
-		firstImage.src = "/img/balotelli.jpg";
-		ctx.fillText("Balotelli 1", 620, 50);
-
-		// 2nd place
-		var secondReady = false;
-		var secondImage = new Image();
-		secondImage.onload = function () {
-			ctx.drawImage(secondImage, 375, 200);
-		};
-		secondImage.src = "/img/balotelli.jpg";
-		ctx.fillText("Balotelli 2", 410, 150);
-
-		// 3rd place
-		var thirdReady = false;
-		var thirdImage = new Image();
-		thirdImage.onload = function () {
-			ctx.drawImage(thirdImage, 780, 250);
-		};
-		thirdImage.src = "/img/balotelli.jpg";
-		ctx.fillText("Balotelli 3", 815, 200);
-		
-	};
-	
-	var animate = function () {
-		log("animate");
-	};
-	
-	return {
-		
-		init : function () {
-			
-			function canvasSupport() {
-				return !!document.createElement('canvas').getContext;
-			}
-
-			function eventWindowLoaded() {
-
-				if (!canvasSupport) {
-					return;
-				}
-
-//				render();
-				drawPodium();
-				drawImages();
-				animate();
-			}
-			
-			window.addEventListener('load', eventWindowLoaded, false);
-			
+		function canvasSupport() {
+			return !!document.createElement('canvas').getContext;
 		}
-	
-	};
 
-}());
+		function eventWindowLoaded() {
+
+			if (!canvasSupport) {
+				return;
+			}
+
+			Podium.renderBg();
+		}
+		
+		window.addEventListener('load', eventWindowLoaded, false);
+	},
+	
+	renderBg : function () {
+		Podium.background = new Image();
+		Podium.background.onload = function () {
+			Podium.ctx.drawImage(Podium.background, 0, 0);
+			Podium.animateBg();
+		};
+		Podium.background.src = '/img/podium.png';
+	},
+	
+	clearCanvas : function () {
+		Podium.ctx.clearRect(0, 0, Podium.width, Podium.height);
+	},
+	
+	rect : function (x, y, w, h) {
+		Podium.ctx.beginPath();
+		Podium.ctx.rect(x ,y, w, h);
+		Podium.ctx.closePath();
+		Podium.ctx.fill();
+		Podium.ctx.stroke();
+	},
+	
+	animateBg : function () {
+		if (Podium.canvasY <= Podium.canvasMaxY) {
+          requestAnimationFrame(Podium.animateBg);
+          Podium.clearCanvas();
+          Podium.rect(0, 0, Podium.width, Podium.height);
+          Podium.ctx.drawImage(Podium.background, 0, parseInt("-" + (++Podium.canvasY), 10));
+		} else {
+			Podium.drawImages();
+		}
+	},
+	
+	drawImages : function () {
+		Podium.third = new Image();
+		Podium.third.onload = function () {
+			Podium.ctx.drawImage(Podium.third, 545, 310);
+		};
+		Podium.third.src = "/img/balotelli.jpg";
+		
+		Podium.second = new Image();
+		Podium.second.onload = function () {
+			Podium.ctx.drawImage(Podium.second, 310, 290);
+		};
+		Podium.second.src = "/img/balotelli.jpg";
+		
+		Podium.first = new Image();
+		Podium.first.onload = function () {
+			Podium.ctx.drawImage(Podium.first, 430, 230);
+		};
+		Podium.first.src = "/img/balotelli.jpg";
+	}
+	
+};
 
 Podium.init();
