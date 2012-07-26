fce33c972c2c58db2957995edf2285153f58a040
Shaun Baker <shaun@shaunbakerdesigns.co.uk>
2012-07-26 16:54:43 +0100

    Share buttons

--- public/js/common.js
+++ public/js/common.js
@@ -52,3 +52,14 @@ window.fbAsyncInit = function() {
  js.src = "//connect.facebook.net/en_US/all.js";
  ref.parentNode.insertBefore(js, ref);
 }(document));
+
+
+(function() {
+  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+  po.src = 'https://apis.google.com/js/plusone.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
+
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
